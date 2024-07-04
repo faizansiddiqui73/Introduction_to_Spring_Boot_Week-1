bcc73e7756563825bb8b6ca09226fdea822aee40
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2024-07-05 00:56:15 +0530

    1.2 setting up

---
+++ src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.com.faizan.week1Introduction.IntroductionToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.com.faizan.week1Introduction.IntroductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

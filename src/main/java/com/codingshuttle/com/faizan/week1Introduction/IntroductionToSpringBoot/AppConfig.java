34e5920b5d3658d1822bf54a9275a1bc7a2fa79b
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2024-07-05 20:45:27 +0530

    1.2 Creation of Bean by Using Stereotype Annotations, and Explicit Bean Declaration in Configuration Class

---
+++ src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/AppConfig.java
@@ -0,0 +1,17 @@
+package com.codingshuttle.com.faizan.week1Introduction.IntroductionToSpringBoot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//This is the expilicit based creation of bean Now we need to remove the component annotation in apple class
+//coz 2 beans are found
+@Configuration // Bean is built and it is injected in IntroductionToSpringBootApplication
+public class AppConfig {
+
+    @Bean
+    Apple getApple() {
+        return new Apple();
+    }
+    //Above method the developer is taking care of creation of object not the spring framework
+    // The spring framework will invoke the and inject the dependency.
+}
--- src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/Apple.java
+++ src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/Apple.java
@@ -3,8 +3,8 @@ package com.codingshuttle.com.faizan.week1Introduction.IntroductionToSpringBoot;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+//Streotype Method for beans
 
-@Component
 public class Apple {
     void eatApple() {
         System.out.println("I am eating an apple");

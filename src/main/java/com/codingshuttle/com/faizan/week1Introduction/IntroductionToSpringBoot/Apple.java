29c6add53a368fe20f9bf04273b50aaf5fe32363
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2024-07-05 20:36:07 +0530

    1.2 Working of  Dependency Injection

---
+++ src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/Apple.java
@@ -0,0 +1,12 @@
+package com.codingshuttle.com.faizan.week1Introduction.IntroductionToSpringBoot;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
+@Component
+public class Apple {
+    void eatApple() {
+        System.out.println("I am eating an apple");
+    }
+}
--- src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/IntroductionToSpringBootApplication.java
+++ src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -1,13 +1,29 @@
 package com.codingshuttle.com.faizan.week1Introduction.IntroductionToSpringBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionToSpringBootApplication {
+public class IntroductionToSpringBootApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
-	}
+    // SpringBootApplication is also a bean
+    //Apple is also a bean
+    //Therefore the IntroductionToSpringBootApplication Bean is dependent on Apple Bean to that apple bean will inject it wherever
+    // it is required.
+    //Therefor the Apple is dependency injection for the IntroductionToSpringBootApplication
+    @Autowired
+    Apple obj;
 
+    public static void main(String[] args) {
+
+        SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        obj.eatApple();
+    }
 }

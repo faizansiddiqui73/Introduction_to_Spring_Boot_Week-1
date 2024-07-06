0506d449592f6d4bb9c6d5b07a8a589cc9ef188c
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2024-07-06 17:32:46 +0530

    1.4 @Primary Annotation helping for which bean will inject, but intface does not solve the peorblem of tight coupling anyways we we are marking our bean whihc on should run

--- src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/ProdDB.java
+++ src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/ProdDB.java
@@ -1,8 +1,10 @@
 package com.codingshuttle.com.faizan.week1Introduction.IntroductionToSpringBoot;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class ProdDB implements DB {
     public String getData() {
         return "Production Data";

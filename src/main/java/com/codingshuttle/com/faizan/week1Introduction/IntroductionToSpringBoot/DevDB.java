df8bf6fb2088d3428c0f0b9dd87d5ac635c32088
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2024-07-06 17:56:04 +0530

    1.4 Achived Loose Coupling with help of Configuration in application properties and explicitly telling the component

--- src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/DevDB.java
+++ src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/DevDB.java
@@ -1,8 +1,10 @@
 package com.codingshuttle.com.faizan.week1Introduction.IntroductionToSpringBoot;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "deploy.env", havingValue = "development")
 public class DevDB implements DB {
 
     @Override
--- src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/ProdDB.java
+++ src/main/java/com/codingshuttle/com/faizan/week1Introduction/IntroductionToSpringBoot/ProdDB.java
@@ -1,10 +1,10 @@
 package com.codingshuttle.com.faizan.week1Introduction.IntroductionToSpringBoot;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@ConditionalOnProperty(name = "deploy.env",havingValue = "production")
 public class ProdDB implements DB {
     public String getData() {
         return "Production Data";
